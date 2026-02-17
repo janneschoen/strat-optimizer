7dcf9b6335d24520dc5f7f7be2794ee226eee7ef
Janne <schoen.janne@gmail.com>
2026-02-17 18:20:16 +0100

    functional demo trading

--- tradingBot/main.py
+++ tradingBot/main.py
@@ -5,83 +5,138 @@ import time
 import logging
 from datetime import datetime
 import importlib
+import inspect
 
-parser = argparse.ArgumentParser(description='Run trading strategy with specific parameters.')
-parser.add_argument("-s", required=True, type=int, help='strategy ID')
-parser.add_argument('-p', nargs='+', type=int, help='strategy parameters')
-parser.add_argument('-a', required=True, type=str, help='asset ticker')
+def buy(bitget, symbol, quantity):
 
-args = parser.parse_args()
-symbol = args.a
-stratID = args.s
-params = args.p
+    params = {
+        "marginMode": "isolated",
+    }
 
-try:
-    strategy = importlib.import_module(f"strategies.strat{stratID}")
-except:
-    print("Error importing strategy.")
-    exit(1)
-
-with open('keys.json', 'r') as file:
-    keys = json.load(file)
-
-apiKey = keys["apikey"]
-secret = keys["secret"]
-password = keys["password"]
-
-bitget = ccxt.bitget({
-    'apiKey': apiKey,
-    'secret': secret,
-    'password': password,
-    'headers': {
-        'paptrading': '1',
-    },
-    'options': {
-        'defaultType': 'future',
-    },
-})
-
-logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+    bitget.create_order(f"{symbol}:USDT", type='market', amount=quantity, side='buy', params=params)
 
 
-while True:
-    try:
-        now = datetime.now()
+def sell(bitget, symbol, quantity):
 
-        balance = bitget.fetch_balance()
-        positions = bitget.fetch_positions()
+    params = {
+        "marginMode": "isolated",
+    }
 
-        networth = balance["total"]["USDT"]
-        cash = balance["free"]["USDT"]
+    bitget.create_order(f"{symbol}:USDT", type='market', amount=quantity, side='sell', params=params)
 
-        if(len(positions) > 0):
-            position = positions[0]["side"]
+
+def takePosition(bitget, signal, symbol, allocation):
+        
+    ticker = bitget.fetch_ticker(symbol)
+    currentPrice = ticker['last']
+
+    portfolio = getPortfolio(bitget)
+    networth = portfolio[0]
+    invested = portfolio[1]
+    side = portfolio[2]
+
+    desiredInv = networth * allocation
+
+    markets = bitget.load_markets()
+
+    if signal == side:
+        if abs(desiredInv - invested) / currentPrice <= 0.0001:
+            logging.info(f"({side}, {invested}) - maintaining position")
+            print(f"({side}, {invested}) - maintaining position")
+            return
+
+        if (signal == "long" and desiredInv > invested) or (signal == "short" and desiredInv < invested):
+            toBuy = (desiredInv - invested) / currentPrice
+            buy(bitget, symbol, toBuy)
         else:
-            position = None
-        
-        logging.info(f"Net: {networth} | Cash: {cash} | Pos: {position}")
+            toSell = (invested - desiredInv) / currentPrice
+            sell(bitget, symbol, toSell)
+    else:
+        if signal == "long":
+            toBuy = (desiredInv + invested) / currentPrice
+            buy(bitget, symbol, toBuy)
+        else:
+            toSell = (desiredInv + invested) / currentPrice
+            sell(bitget, symbol, toSell)
+    
+    logging.info(f"({side}, {invested}) -> ({signal}, {desiredInv})")
+    print(f"({side}, {invested}) -> ({signal}, {desiredInv})")
 
-        signal = strategy.generateSignal(bitget, params, symbol)
 
-        if signal != position:
-            bitget.load_markets()
+def getPortfolio(bitget):
+    balance = bitget.fetch_balance()
+    positions = bitget.fetch_positions()
 
-            asset = bitget.fetch_ticker(symbol)
-            currentPrice = asset['last']
+    networth = balance["total"]["USDT"]
+    invested = balance["used"]["USDT"]
 
-            investment = (networth / 10) / currentPrice
+    if(len(positions) > 0):
+        position = positions[0]["side"]
+    else:
+        position = None
+    
+    return networth, invested, position
 
-            if signal == 'long':
-                #close short pos
-                #open long pos
-                pass
-            elif signal == 'short':
-                #close long pos
-                #open short pos
-                pass
-        
-    except Exception as e:
-        print(f'ERROR: {e}')
-        logging.error(e)
+def runStrategy(bitget, strategy, symbol, params, allocation):
 
-    time.sleep(120)
+    signal = strategy.generateSignal(bitget, params, symbol)
+
+    takePosition(bitget, signal, symbol, allocation)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run trading strategy with specific parameters.')
+    parser.add_argument('-s', required=True, type=int, help='strategy ID')
+    parser.add_argument('-p', nargs='+', type=int, help='strategy parameters')
+    parser.add_argument('-i', required=True, type=float, help='investment size')
+    parser.add_argument('-a', required=True, type=str, help='asset ticker')
+
+    args = parser.parse_args()
+    symbol = args.a
+    stratID = args.s
+    params = args.p
+    allocation = args.i
+
+    try:
+        strategy = importlib.import_module(f"strategies.strat{stratID}")
+    except:
+        print("Error importing strategy.")
+        exit(1)
+
+    with open('keys.json', 'r') as file:
+        keys = json.load(file)
+
+    apiKey = keys["apikey"]
+    secret = keys["secret"]
+    password = keys["password"]
+
+    bitget = ccxt.bitget({
+        'apiKey': apiKey,
+        'secret': secret,
+        'password': password,
+        'headers': {
+            'paptrading': '1',
+        },
+        'options': {
+            'defaultType': 'future',
+        },
+    })
+
+    elem = bitget.fetch_canceled_and_closed_orders()
+    
+
+    logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+
+
+    while True:
+        try:
+            runStrategy(bitget, strategy, symbol, params, allocation)
+            
+        except Exception as e:
+            print(f'ERROR: {e}')
+            logging.error(e)
+
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
