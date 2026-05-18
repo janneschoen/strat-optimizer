eb9381ea1d7b11a23c0843270913c69e6e3edd89
Janne <schoen.janne@gmail.com>
2026-05-18 21:11:45 +0200

    separate conditions for readability

--- main.py
+++ main.py
@@ -16,12 +16,27 @@ class StrategyType:
     def isValid(self, p):
         if len(p) != self.numParams:
             return False
+
         if self.name == "SMA Crossover":
-            if p[0] >= p[1] or p[1] < 2 or p[2] <= 0 or p[2] > 1:
+            if p[0] >= p[1]:
                 return False
+            if p[1] < 2:
+                return False
+            if p[2] <= 0:
+                return False
+            if p[2] > 1:
+                return False
+
         if self.name == "RSI":
-            if p[2] < 2 or p[0] < 0 or p[1] > 100 or p[0] > p[1]:
+            if p[2] < 2:
                 return False
+            if p[0] < 0:
+                return False
+            if p[1] > 100:
+                return False
+            if p[0] > p[1]:
+                return False
+
         return True
 
 def main():
